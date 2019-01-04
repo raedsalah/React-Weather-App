83c67f9f7c328972663d436f72ac3138ccae1832
Raed Salah <dkraed@gmail.com>
2019-01-04 13:52:29 +0800

    weather compenent created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Titles from "./components/titles";
 import Form from "./components/form";
+import Weather from "./components/weather";
 
 class App extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class App extends React.Component {
       <div>
         <Titles />
         <Form />
+        <Weather />
       </div>
     );
   }
---
+++ src/components/weather.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Weather extends Component {
+  state = {};
+  render() {
+    return <div>Weather Component</div>;
+  }
+}
+
+export default Weather;
