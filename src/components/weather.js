a4fa3675bebb30b4dcb94d8df35c6b264a776c20
Raed Salah <dkraed@gmail.com>
2019-01-05 14:19:05 +0800

    styling completed

--- src/components/weather.js
+++ src/components/weather.js
@@ -2,18 +2,28 @@ import React from "react";
 
 const Weather = props => {
   return (
-    <div>
+    <div className="weather__info">
       {props.country && props.city ? (
         <div>
-          <p>
-            Location: {props.country}, {props.city}
+          <p className="weather__key">
+            Location:{" "}
+            <span className="weather__value">
+              {props.country}, {props.city}
+            </span>
           </p>
-          <p>Temperature: {props.temperature}</p>
-          <p>Humidity: {props.humidity}</p>
-          <p>Condition: {props.desc}</p>
+          <p className="weather__key">
+            Temperature:{" "}
+            <span className="weather__value">{props.temperature}</span>
+          </p>
+          <p className="weather__key">
+            Humidity: <span className="weather__value">{props.humidity}</span>
+          </p>
+          <p className="weather__key">
+            Condition: <span className="weather__value">{props.desc}</span>
+          </p>
         </div>
       ) : (
-        <div>{props.error}</div>
+        <p className="weather__error">{props.error}</p>
       )}
     </div>
   );
